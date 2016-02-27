ed14acbefd7069a743ba531c7d6c44b2bd11b600
LaTonya Harrison <allone1@users.noreply.github.com>
2016-02-27 16:48:13 -0700

    Refactor code to create reusable addCommentFromInputBox function with 2 event listeners

--- Example2/app.js
+++ Example2/app.js
@@ -1,8 +1,8 @@
 // JavaScript Document for Chapter4/Example2
 var main = function () {
 	"use strict";
-		
-	$(".comment-input button").on("click", function (event) {
+	
+	var addCommentFromInputBox = function () {
 		var $new_comment;
 		
 		if ($(".comment-input input").val() !== "") {
@@ -11,25 +11,22 @@ var main = function () {
 			$(".comments").append($new_comment);  //appends new comment to comments element
 			$new_comment.fadeIn();
 			$(".comment-input input").val(""); //clears out the input field text
+		
 		} 
-		
-		
-	});
+	};
 	
+	
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox ();			
+				
+	});
+		
 	$(".comment-input input").on("keypress", function (event) {
-		var $new_comment;
-		
 		if (event.keyCode === 13) {
-		
-			if	($(".comment-input input").val() !== "") {
-				$new_comment = $("<p>").text($(".comment-input input").val()); 
-				$new_comment.hide();
-				$(".comments").append($new_comment);  
-				$new_comment.fadeIn();
-				$(".comment-input input").val(""); 
-			}
+			addCommentFromInputBox ();
+				
 		}
-		
+							
 	});
 	
 };
