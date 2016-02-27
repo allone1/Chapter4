f4a9449869cd5549e32fde9b1a85e5f72831161e
LaTonya Harrison <allone1@users.noreply.github.com>
2016-02-27 09:25:40 -0700

    Re-add initial remote after original initial commit was not recorded.

---
+++ Example1/app.js
@@ -0,0 +1,10 @@
+// JavaScript Document for Chapter4/Example1
+var main = function () {
+	"use strict";
+	
+	window.alert("hello world!");
+	
+};
+
+$(document).ready(main);
+
---
+++ Example2/app.js
@@ -0,0 +1,20 @@
+// JavaScript Document for Chapter4/Example2
+var main = function () {
+	"use strict";
+		
+	$(".comment-input button").on("click", function (event) {
+		var $new_comment;
+		
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val()); 
+			$(".comments").append($new_comment);
+		} 
+		
+		
+		
+	});
+	
+};
+
+$(document).ready(main);
+
