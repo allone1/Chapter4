172598cbce0dcddebdbb1964b45e5b22a88c20c8
LaTonya Harrison <allone1@users.noreply.github.com>
2016-02-27 16:36:41 -0700

    Add cpde to append new comment when Enter key pressed, add comment hide and fade in

--- Example2/app.js
+++ Example2/app.js
@@ -22,11 +22,11 @@ var main = function () {
 		if (event.keyCode === 13) {
 		
 			if	($(".comment-input input").val() !== "") {
-				$new_comment = $("<p>").text($(".comment-input input").val()); //adds new jquery element p and assigns it value user entered in input field.
+				$new_comment = $("<p>").text($(".comment-input input").val()); 
 				$new_comment.hide();
-				$(".comments").append($new_comment);  //appends new comment to comments element
+				$(".comments").append($new_comment);  
 				$new_comment.fadeIn();
-				$(".comment-input input").val(""); //clears out the input field text
+				$(".comment-input input").val(""); 
 			}
 		}
 		
