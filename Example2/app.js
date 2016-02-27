90af3f484d655f3abac8fd7ff4cf0f85d32e3041
LaTonya Harrison <allone1@users.noreply.github.com>
2016-02-27 16:26:25 -0700

    readd example2 folder

--- Example2/app.js
+++ Example2/app.js
@@ -7,10 +7,28 @@ var main = function () {
 		
 		if ($(".comment-input input").val() !== "") {
 			$new_comment = $("<p>").text($(".comment-input input").val()); //adds new jquery element p and assigns it value user entered in input field.
+			$new_comment.hide();
 			$(".comments").append($new_comment);  //appends new comment to comments element
+			$new_comment.fadeIn();
 			$(".comment-input input").val(""); //clears out the input field text
 		} 
 		
+		
+	});
+	
+	$(".comment-input input").on("keypress", function (event) {
+		var $new_comment;
+		
+		if (event.keyCode === 13) {
+		
+			if	($(".comment-input input").val() !== "") {
+				$new_comment = $("<p>").text($(".comment-input input").val()); //adds new jquery element p and assigns it value user entered in input field.
+				$new_comment.hide();
+				$(".comments").append($new_comment);  //appends new comment to comments element
+				$new_comment.fadeIn();
+				$(".comment-input input").val(""); //clears out the input field text
+			}
+		}
 		
 	});
 	
@@ -18,3 +36,4 @@ var main = function () {
 
 $(document).ready(main);
 
+   
